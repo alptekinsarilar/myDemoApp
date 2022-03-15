f9e66eb755ea9dd0814d792a4f16d7fead21f0c8
Alptekin <alptekinsarilar@gmail.com>
2022-03-15 22:53:47 +0300

    shadowJar activated

--- app/src/main/java/myDemoApp/App.java
+++ app/src/main/java/myDemoApp/App.java
@@ -33,8 +33,8 @@ public class App {
 
         post("/compute", 
              (req, res) -> {
-                System.out.println(req.queryParams("input1"));
-                System.out.println(req.queryParams("input2"));
+                System.out.println("input1: " + req.queryParams("input1"));
+                System.out.println("input2: " + req.queryParams("input2"));
     
                 String input1 = req.queryParams("input1");
                 java.util.Scanner sc1 = new java.util.Scanner(input1);
